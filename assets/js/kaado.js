29a769302176b278353eecbc80414c27c096ebef
Eric Akaoka <eric.akaoka@gmail.com>
2017-02-04 12:57:32 -0500

    Basic drag functionality (works on mobile too, thanks to https://github.com/furf/jquery-ui-touch-punch/blob/master/jquery.ui.touch-punch.min.js).

--- assets/js/kaado.js
+++ assets/js/kaado.js
@@ -1,5 +1,11 @@
 
 
 $(document).ready(function() {
-	
+	$(".card").draggable();
+	$( ".card" ).on( "dragstart", function( event, ui ) {
+		ui.helper.removeClass("facedown");
+	} );	
+	$( ".card" ).on( "dragstop", function( event, ui ) {
+		ui.helper.addClass("facedown");
+	} );	
 });
